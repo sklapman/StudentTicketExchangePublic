6e798970461c31ac852186c2d66c4980f68bbb00
markdouple <mdouple@umich.edu>
2019-11-12 09:19:00 -0500

    mark test

--- app/src/main/java/com/example/studentticketexchange/MainActivity.java
+++ app/src/main/java/com/example/studentticketexchange/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //test seth
+    //test mark
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
