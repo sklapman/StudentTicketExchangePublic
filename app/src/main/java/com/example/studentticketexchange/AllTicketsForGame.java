96110e33a27cbb6f35c6f8cc9ac52758872ca210
sklapman <klapman.seth@gmail.com>
2019-11-12 11:21:34 -0500

    add schedule activity

---
+++ app/src/main/java/com/example/studentticketexchange/AllTicketsForGame.java
@@ -0,0 +1,14 @@
+package com.example.studentticketexchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllTicketsForGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_tickets_for_game);
+    }
+}
---
+++ app/src/main/java/com/example/studentticketexchange/SellTicketDetails.java
@@ -0,0 +1,14 @@
+package com.example.studentticketexchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellTicketDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_ticket_details);
+    }
+}
