0ee4cff0038687424e44baa1d5b8fb11f04ef123
sklapman <klapman.seth@gmail.com>
2019-11-12 09:17:24 -0500

    seth added

--- app/src/main/java/com/example/studentticketexchange/MainActivity.java
+++ app/src/main/java/com/example/studentticketexchange/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //test seth
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
