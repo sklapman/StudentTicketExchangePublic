12f1a91dde6c607123c792a98ffa747182426150
sklapman <klapman.seth@gmail.com>
2019-11-19 19:47:24 -0500

    add schedule activity

--- app/src/main/java/com/example/studentticketexchange/Profile.java
+++ app/src/main/java/com/example/studentticketexchange/Profile.java
@@ -3,12 +3,22 @@ package com.example.studentticketexchange;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Profile extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        mMainNav = (BottomNavigationView) findViewById(R.id.id_Navbar);
+        mMainFrame = (FrameLayout) findViewById(R.id.id_frame);
+
     }
 }
