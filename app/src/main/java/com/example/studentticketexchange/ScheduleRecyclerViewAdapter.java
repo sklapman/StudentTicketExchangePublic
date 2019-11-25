f35541b1ab359da6869a7736735189fab99d2142
seansiry <sean.siry@gmail.com>
2019-11-25 15:36:27 -0500

    Schedule recyclerView

--- app/src/main/java/com/example/studentticketexchange/ScheduleRecyclerViewAdapter.java
+++ app/src/main/java/com/example/studentticketexchange/ScheduleRecyclerViewAdapter.java
@@ -1,15 +1,22 @@
 package com.example.studentticketexchange;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class ScheduleRecyclerViewAdapter extends RecyclerView.Adapter<ScheduleRecyclerViewAdapter.ScheduleViewHolder> {
+    private ArrayList<Game> games = new ArrayList<>();
+    private Context mContext;
+
     @NonNull
     @Override
     public ScheduleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -20,12 +27,25 @@ public class ScheduleRecyclerViewAdapter extends RecyclerView.Adapter<ScheduleRe
 
     @Override
     public void onBindViewHolder(@NonNull ScheduleViewHolder holder, int position) {
-
+        Game game = games.get(position);
+        String[] months = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        holder.dateText.setText(months[game.month - 1] + " " + game.day);
+        holder.opponentText.setText(game.opponent);
+        holder.availableText.setText("");
+        holder.minPriceText.setText("");
+        holder.avgPriceText.setText("");
+        holder.maxPriceText.setText("");
+        holder.gameItemParent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, "Now would navigate to ticket options", Toast.LENGTH_SHORT);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return games.size();
     }
 
     public class ScheduleViewHolder extends RecyclerView.ViewHolder{
@@ -39,7 +59,7 @@ public class ScheduleRecyclerViewAdapter extends RecyclerView.Adapter<ScheduleRe
             minPriceText = itemView.findViewById(R.id.textView_game_price_min);
             avgPriceText = itemView.findViewById(R.id.textView_game_price_avg);
             maxPriceText = itemView.findViewById(R.id.textView_game_price_max);
-
+            gameItemParent = itemView.findViewById(R.id.gameItem_layout);
         }
     }
 }
