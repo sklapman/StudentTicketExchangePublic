b10feeb39c891e2ef4f7ef7d078326c74616fe1d
Ryan Manes <rymanes@gmail.com>
2019-11-25 00:09:54 -0500

    Merge branch 'master' of C:\Users\ryman\Documents\08. Fall B 2019\TO 626 Fall B\StudentTicketExchangePublic with conflicts.

---
+++ app/src/main/java/com/example/studentticketexchange/RecyclerViewAdapterAllTix.java
@@ -0,0 +1,17 @@
+package com.example.studentticketexchange;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapterAllTix {
+
+    public class AllTixViewHolder extends RecyclerView.ViewHolder {
+
+        public AllTixViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/studentticketexchange/RecyclerViewTest.java
@@ -0,0 +1,14 @@
+package com.example.studentticketexchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecyclerViewTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view_test);
+    }
+}
