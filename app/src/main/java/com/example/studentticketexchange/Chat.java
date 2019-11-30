ffcafebbf8726a8e8efd61745ea26c83dae95214
KP <kprzybylak100@gmail.com>
2019-11-30 03:45:25 -0500

    Chat v1

--- app/src/main/java/com/example/studentticketexchange/Chat.java
+++ app/src/main/java/com/example/studentticketexchange/Chat.java
@@ -5,15 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Chat extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    String TestUser1;
+    String TestUser2;
+
+    LinearLayout layout;
+    RelativeLayout layout_2;
+    ImageView sendButton;
+    EditText messageArea;
+    ScrollView scrollView;
+    Firebase reference1, reference2;
 
     private BottomNavigationView mMainNav;
     private FrameLayout mMainFrame;
@@ -28,6 +54,97 @@ public class Chat extends AppCompatActivity implements BottomNavigationView.OnNa
         mMainFrame = (FrameLayout) findViewById(R.id.id_frame);
 
         mMainNav.setOnNavigationItemSelectedListener(this);
+
+        // TEST USERS
+        TestUser1 = "aaaaaa";
+        TestUser2 = "bbbbbb";
+
+        layout = (LinearLayout) findViewById(R.id.layout1);
+        layout_2 = (RelativeLayout)findViewById(R.id.layout2);
+        sendButton = (ImageView)findViewById(R.id.sendButton);
+        messageArea = (EditText)findViewById(R.id.messageArea);
+        scrollView = (ScrollView)findViewById(R.id.scrollView);
+
+        Firebase.setAndroidContext(this);
+        //reference1 = new Firebase("https://studentticketsexchange.firebaseio.com/messages/" + UserDetails.username + "_" + UserDetails.chatWith);
+       //reference2 = new Firebase("https://studentticketsexchange.firebaseio.com/messages/" + UserDetails.chatWith + "_" + UserDetails.username);
+
+        reference1 = new Firebase("https://studentticketsexchange.firebaseio.com/messages/" + TestUser1 + "_" + TestUser2);
+        reference2 = new Firebase("https://studentticketsexchange.firebaseio.com/messages/" + TestUser2 + "_" + TestUser1);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = messageArea.getText().toString();
+
+                if(!messageText.equals("")){
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("message", messageText);
+                    map.put("user", UserDetails.username);
+                    reference1.push().setValue(map);
+                    reference2.push().setValue(map);
+                    messageArea.setText("");
+                }
+            }
+        });
+
+        reference1.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map map = dataSnapshot.getValue(Map.class);
+                String message = map.get("message").toString();
+                String userName = map.get("user").toString();
+                // if(userName.equals(UserDetails.username)){
+                if(userName.equals(TestUser1)){
+                    addMessageBox("You:-\n" + message, 1);
+                }
+                else{
+                    // addMessageBox(UserDetails.chatWith + ":-\n" + message, 2);
+                    addMessageBox(TestUser2 + ":-\n" + message, 2);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+    }
+
+    public void  addMessageBox (String message, int type) {
+        TextView textView = new TextView(Chat.this);
+        textView.setText(message);
+
+        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp2.weight = 1.0f;
+
+        if(type == 1) {
+            lp2.gravity = Gravity.LEFT;
+            textView.setBackgroundResource(R.drawable.bubble_in);
+        }
+        else{
+            lp2.gravity = Gravity.RIGHT;
+            textView.setBackgroundResource(R.drawable.bubble_out);
+        }
+        textView.setLayoutParams(lp2);
+        layout.addView(textView);
+        scrollView.fullScroll(View.FOCUS_DOWN);
     }
 
     @Override
---
+++ app/src/main/java/com/example/studentticketexchange/UserDetails.java
@@ -0,0 +1,7 @@
+package com.example.studentticketexchange;
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
